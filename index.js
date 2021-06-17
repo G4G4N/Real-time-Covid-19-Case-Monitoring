fefc80d4ea1edf7315ee84f1859da8219d847735
G4G4N <56641054+G4G4N@users.noreply.github.com>
2021-06-17 11:40:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+$.ajax({
+
+    type : 'get',
+    url : 'https://api.covid19api.com/summary',
+    success : function(response)
+    {
+
+        for(var i=0; i<response.Countries.length ; i++)
+        {
+            var TotalActive = response.Countries[i].TotalConfirmed - response.Countries[i].TotalRecovered  
+            var tablerow = `<tr> 
+                                <td>${response.Countries[i].Country}</td> 
+                                <td>${response.Countries[i].TotalConfirmed}</td> 
+                                <td>${TotalActive}</td> 
+                                <td>${response.Countries[i].TotalRecovered}</td> 
+                                <td>${response.Countries[i].TotalDeaths}</td> 
+                            </tr>`
+            $('#tbody').append(tablerow)
+        }
+
+        $('#covidtable').DataTable()
+    },
+    error: function(error)
+    {
+        console.log(error)
+    }
+})
